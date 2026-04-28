6700324c15d1b59132ecbdc66f0e2504a342359a
Kmaiers19 <Maiersk200@gmail.com>
2026-04-28 17:25:20 -0400

    Import React in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import HRRPConsole from "./HRRPConsole.jsx";
 
 createRoot(document.getElementById("root")).render(<HRRPConsole />);
